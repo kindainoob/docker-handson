5e46efa28f39f570830b24e822b13a721ee1247c
kindainoob <kindai.noob@gmail.com>
2019-11-20 00:57:59 +0900

    written comment

--- go/main.go
+++ go/main.go
@@ -12,7 +12,9 @@ type Ping struct {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-    ping := Ping{http.StatusOK, "ok"}
+    ping := Ping{http.StatusOK, "ok"} // 変更する
+    // ping := Ping{http.StatusOK, "hello"} //　変更した
+    // docker.htmlで受け取るjsonが変わって表示も変わる
     res, _ := json.Marshal(ping)
 
 	w.Header().Set("Content-Type", "application/json")
